9c5634f8ae79e58fb3f92559158328611001b436
sioncamara <sioncamara@gmail.com>
2025-06-25 15:46:20 -0500

    init commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+import avatar from '@/images/avatar.ico'
+
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Sion Wilks',
+    default: 'Sion Wilks - Philosophical Software Engineer',
+  },
+  description:
+    "I'm a front-end software engineer who spends much of my time reading and thinking about philosophy and psychology.",
+  icons: {
+    icon: avatar.src,
+  },
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
